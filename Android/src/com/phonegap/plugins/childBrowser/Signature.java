cf04c6ab9b21cd0a6744f654191d1e4b7b0e5880
ricaisme@gmail.com <ricaisme@gmail.com@f18102c3-c135-b6c4-cfbb-9b241380d991>
2012-07-03 22:52:00 +0000

    A native plugin for signature with java code added, it should be much faster than a pure javascript library.

---
+++ Android/src/com/phonegap/plugins/childBrowser/Signature.java
@@ -0,0 +1,22 @@
+package com.phonegap.plugins.childBrowser;
+
+import org.apache.cordova.api.Plugin;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+import android.content.Context;
+import android.graphics.*;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Signature extends Plugin {
+
+	@Override
+	public PluginResult execute(String arg0, JSONArray arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
